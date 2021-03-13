42206de8a8069fdd214512df5691f7d761644ff0
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-13 14:39:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^0-9a-z- ']/gi, "");
+  }
+
+  static titleize(string) {
+    let arr = string.split(" ");
+    let titleizeString = arr
+      .map(function (el) {
+        if (
+          el !== "the" &&
+          el !== "a" &&
+          el !== "an" &&
+          el !== "but" &&
+          el !== "of" &&
+          el !== "and" &&
+          el !== "for" &&
+          el !== "at" &&
+          el !== "by" &&
+          el !== "from"
+        ) {
+          //utilize my capitalize helper
+          return Formatter.capitalize(el);
+        } else {
+          return el;
+        }
+      })
+      .join(" ");
+    return Formatter.capitalize(titleizeString);
+  }
 }
